f626e0dd7ed41331097cfec8da3b4afe292872d4
cardaozel <arda.civan@gmail.com>
2024-11-25 04:35:44 +0300

    connect the image and the labels to class

--- SimpsonBook/detailsVC.swift
+++ SimpsonBook/detailsVC.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class detailsVC: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
