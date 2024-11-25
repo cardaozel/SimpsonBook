9bca3425aef0c6c02e572591191338629817f9f7
cardaozel <arda.civan@gmail.com>
2024-11-26 02:20:18 +0300

    tableview creating

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -7,14 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Homer Simpson"
+        return cell
+    }
+    
 
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    
     }
 
 
--- SimpsonBook/detailsVC.swift
+++ SimpsonBook/detailsVC.swift
@@ -10,9 +10,7 @@ import UIKit
 class detailsVC: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var jobLabel: UILabel!
     
     override func viewDidLoad() {
