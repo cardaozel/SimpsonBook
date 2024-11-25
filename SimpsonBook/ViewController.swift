48131ccfaf087658ff9e208918f9c7a286b814da
cardaozel <arda.civan@gmail.com>
2024-11-26 02:44:49 +0300

    write controller name

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -9,19 +9,10 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
-   
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
+   @IBOutlet weak var tableView: UITableView!
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Homer Simpson"
-        return cell
-    }
+    var mySimpsons = [Simpson]()
     
-
-    @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
@@ -30,8 +21,37 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         tableView.dataSource = self
         tableView.delegate = self
     
+ 
+    
+
+    //Simpson Objetcs
+    
+    let homer = Simpson(simpsonName: "Homer Simpson", simpsonJob: "Nuclear Safety", simpsonImage: UIImage(named: "homer")!)
+    let marge = Simpson(simpsonName: "Marge Simpson", simpsonJob: "Housewife", simpsonImage: UIImage(named: "marge")!)
+    let bart = Simpson(simpsonName: "Bart Simpson", simpsonJob: "Student", simpsonImage: UIImage(named: "bart")!)
+    let lisa = Simpson(simpsonName: "Lisa Simpson", simpsonJob: "Student", simpsonImage: UIImage(named: "lisa")!)
+    let maggie = Simpson(simpsonName: "Maggie Simpson", simpsonJob: "baby", simpsonImage: UIImage(named: "maggie")!)
+    let mrburns = Simpson(simpsonName: "Mr. Burns", simpsonJob: "Businessman", simpsonImage:UIImage(named: "mrburns")!)
+    
+    mySimpsons.append(homer)
+    mySimpsons.append(marge)
+    mySimpsons.append(bart)
+    mySimpsons.append(lisa)
+    mySimpsons.append(maggie)
+    mySimpsons.append(mrburns)
+    
+    
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mySimpsons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = mySimpsons[indexPath.row].name
+        return cell
+    }
+    
 }
 
