840e2333de45d6fe99d2658503ff7a5095460394
cardaozel <arda.civan@gmail.com>
2024-11-25 04:30:46 +0300

    fill the detail class

---
+++ SimpsonBook/detailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Arda Özel on 25.11.2024.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
